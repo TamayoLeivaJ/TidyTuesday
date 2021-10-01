69d33b6a3c278aa498a74e5b352b43311e404945
TamayoLeivaJ <j.tamayo.leiva@gmail.com>
2021-10-01 13:09:09 -0300

    Commit #Week 39

--- 2021/2021_Week_038/README.md
+++ 2021/2021_Week_038/README.md
@@ -3,7 +3,7 @@ Contributions to the #TidyTuesday
 
 ## 2021 
 ### Week 038
-#### Billboard Top 100 data set
+#### Billboard Top 100
 ![https://github.com/TamayoLeivaJ/TidyTuesday/blob/gh-pages/2021/2021_Week_038/Plots/2021_Week_038.png](https://github.com/TamayoLeivaJ/TidyTuesday/blob/gh-pages/2021/2021_Week_038/Plots/2021_Week_038.png)
 
 [Code](https://github.com/TamayoLeivaJ/TidyTuesday/blob/gh-pages/2021/2021_Week_038/2021_Week_038.R)
--- 2021/2021_Week_039/2021_Week_039.R
+++ 2021/2021_Week_039/2021_Week_039.R
@@ -2,7 +2,7 @@
 #### ----------------------------------------------------------------------------------------- ####
 ## Emmy Awards data set
 ###  Week     Date	           Data	                 Source	                 Article
-###  - 39     - 2021-09-21     - Emmy Awards         - Emmys                 - Susie Lu
+###  - 39     - 2021-09-21         - Emmy Awards         - Emmys                 - Susie Lu
 
 #### set up ####
 #### Libraries ####
@@ -10,8 +10,9 @@ library(here)
 library(tidytuesdayR)
 library(tidyverse)
 library(ggtext)
+library(circlize)
 library(grid)
-library(ggforce)
+library(patchwork)
 library(systemfonts)
 
 #### Directory ####
@@ -22,84 +23,79 @@ dir.create(here("2021/2021_Week_039/Plots"), recursive = TRUE, mode = "0755")
 #### Read data ####
 tt_data <- tt_load(2021, week = 39)
 
+nominees <- tt_data$nominees
 
 #### Data wrangling #### 
-##### Who are the Formula One constructor with more consecutive World Championship wins?
+##### Which ...?
+emmys <- nominees %>% 
+         separate(category, sep = " - ", into = c("category", "year")) %>% 
+         filter(category %in% c("Outstanding Drama Series","Outstanding Comedy Series","Outstanding Animated Program","Outstanding Variety Talk Series","Outstanding Limited Series")) %>% 
+         mutate(category = str_remove(category, pattern = "Outstanding ")) %>% 
+         filter(distributor %in% c("HBO","NBC","ABC","Netflix","FOX","Hulu","Prime Video")) %>% 
+         group_by(category, distributor, type) %>% 
+         summarise(Counts = n(), .groups = "keep") %>%  
+         group_by(category, distributor) %>% 
+         mutate(Total = sum(Counts),
+                Prop = Counts/Total)
 
-                       
-#### Annotation List ####
+emmys_chord  <- emmys %>% 
+                select(distributor, category, Counts, type) %>% 
+                arrange(type) # So the "Winner" links will be the last on be plotted (on top)
+#### Annotation ####
+
 
 
 #### Text ####
-annotation_title_text <- c("Who are the Formula One constructor with more consecutive World Championship victories?")
-annotation_subtitle_text <- c("The Constructors' Championship was not awarded until 1958. Since then, \nfive constructors' teams have won three or more world championships.")
-
-#### Images ####
-##### Team Logo #####
-
-
-##### Team Cars #####
+annotation_title_text <- c("Emmy Awards")
+annotation_subtitle_text <- c("")
 
 #### Plot aesthetics ####
-background  <- c("#F2F2F3")
-lines_color <- c("#15151E")
-text_color  <- c("#15151E")
-constructor_color <- c("Ferrari" = "#D90404", "McLaren" = "#F28705", "Mercedes" = "#02D2BE", "Red Bull" = "#1600EF", "Williams" = "#0059FF")
+background  <- c("#E1E8EB")
+lines_color <- c("#0AD89D")
+title_color <- c("#152D35")
+subtitle_color <- c("#112031")
+text_color  <- c("#112031")
+caption_color  <- c("#345B63")
 
 #### Plot ####
-     #ggplot(aes(x = year, y = constructor_points)) + 
-     ### Layers base ###
-     #geom_point(aes(fill = constructor_name), size = 2, color = "#696969", alpha = 1/4, shape = 21) +
-     ### Annotations ###
-     ### Clusters Annotations ###
-     #geom_mark_ellipse(aes(label = "(1975–1977)", filter = constructor_name %in% constructor_teams[1] & between(year, 1975, 1977)), label.fill = "transparent", color = constructor_color[1], label.colour = constructor_color[1], con.colour = constructor_color[1], label.buffer = unit(50, 'mm'), expand = unit(2.0, "mm")) +
-     ### Layers extra ###
-     #geom_point(data = race_won_const_year %>% filter(constructor_name %in% constructor_teams[1] & between(year, 1999, 2004)), aes(x = year, y = constructor_points, fill = constructor_name), size = 3, shape = 21) +
-     ### image Annotations ###
-     #annotation_custom(rasterGrob(image = img_Formula1_logo, width = 0.15, x = 0.05, y = -0.1)) +
-     ### Scales ###
-     #scale_x_continuous(breaks = seq(1950, 2020, 5), limits =(c(1950, 2021))) +
-     #scale_y_continuous(expand = c(0, 0.01), breaks = seq(0, 900, 300), limits =(c(0, 950))) +
-     #scale_fill_manual(values = constructor_color) +
-     #coord_cartesian(clip = "off") +
-     ### Guides ###
-     guides(color = guide_legend(title.position = "top", override.aes = list(size = 4)), order=2) +
-     ### Theme ###
-     theme_classic() +
-     theme(
-       ## Text ##
-       ## Axis ##
-       axis.title.x = element_text(face = "plain", color = text_color, size = 12, hjust = 0.5, vjust = 0.5, angle = 0),
-       axis.title.y = element_text(face = "plain", color = text_color, size = 12, hjust = 0.5, vjust = 0.5, angle = 90, margin = margin(t = 0, r = 10, b = 0, l = 0)),
-       axis.text.x = element_text(face = "plain", color = text_color, size = 12, hjust = 0.5, vjust = 0.5, angle = 0),
-       axis.text.y = element_text(face = "plain", color = text_color, size = 12, hjust = 0.5, vjust = 0.5, angle = 0),
-       axis.line.x = element_line(colour = lines_color),
-       axis.ticks.x = element_line(colour = lines_color),
-       axis.line.y = element_line(colour = lines_color),
-       axis.ticks.y = element_line(colour = lines_color),
-       ## Panel Grid ##
-       ## Plot Aesthetic ##
-       panel.background = element_rect(fill = background, color = NA),
-       plot.background = element_rect(fill = background, color = NA),
-       legend.background = element_rect(fill = background, color = NA),
-       legend.key = element_rect(fill = background, color = NA),
-       ## Legend ##
-       legend.position = "none",
-       ## Titles & Caption ##
-       plot.title.position = "plot",
-       plot.title = element_markdown(color = "firebrick4", family = "Avenir", face = "bold"),
-       plot.subtitle = element_markdown(color = "firebrick4", family = "Avenir", face = "plain"),
-       plot.caption = element_markdown(color = "firebrick4", family = "Menlo", hjust = 1, vjust = -55),
-       ## Margin ##
-       plot.margin = margin(t = 0.5, r = 0.5, b = 0.5, l = 0.5, unit = "cm")) +
+p1 <- wrap_elements(full = ~ c(
+              circos.par(gap.after = c("Hulu" = 5, "HBO" = 5, "NBC" = 5, "ABC" = 5, "Netflix" = 5, "Prime Video" = 5, "FOX" = 35,
+                                       "Limited Series" = 5, "Drama Series" = 5, "Comedy Series" = 5, "Animated Program" = 5, "Variety Talk Series" = 35),
+                         start.degree = 67.5),
+              chordDiagram(emmys_chord,
+                           order = c("Limited Series", "Drama Series","Comedy Series","Animated Program","Variety Talk Series", "Hulu","HBO","NBC","ABC","Netflix", "Prime Video","FOX"),
+                           grid.col = c("Hulu" = "#7FC8A9", "HBO" = "#231151FF", "NBC" = "#F47800", "ABC" = "#B3B3B3", "Netflix" = "#CD113B", "Prime Video" = "#0D79AF", "FOX" = "#000000", 
+                                        "Limited Series" = "#E1E8EB", "Drama Series" = "#E1E8EB", "Comedy Series" = "#E1E8EB", "Animated Program" = "#E1E8EB", "Variety Talk Series" = "#E1E8EB"),
+                           transparency = ifelse(emmys_chord$type == "Winner", 0.00, 0.75),
+                           scale = TRUE,
+                           link.sort = TRUE, 
+                           link.decreasing = TRUE,
+                           directional = -1,
+                           diffHeight = mm_h(1),
+                           link.largest.ontop = FALSE)
+)
+                )
+
+
+circos.clear()
+
+plot_emmys  <- 
+p1 + theme(## Plot Aesthetic ##
+           panel.background = element_rect(fill = background, color = NA),
+           plot.background = element_rect(fill = background, color = NA),
+           ## Titles & Caption ##
+           plot.title.position = "plot",
+           plot.title = element_markdown(color = title_color, family = "Basique", face = "bold", size = 16),
+           plot.subtitle = element_markdown(color = subtitle_color, family = "Basique", face = "plain", size = 14),
+           plot.caption = element_markdown(color = caption_color, family = "Menlo", hjust = 1, halign = 0.5),
+           ## Margin ##
+           plot.margin = margin(t = 0.5, r = 0.5, b = 0.5, l = 0.5, unit = "cm")) +
      ### Labels ###
      labs(title = annotation_title_text,
           subtitle = annotation_subtitle_text,
-          x = "Season",
-          y = "Constructor Points by Season",
           caption = "<span style='font-family: \"Font Awesome 5 Brands\"'>&#xf099;</span> @TamayoLeiva_J<br>
-                     <span style='font-family: \"Font Awesome 5 Brands\"'>&#xf09b;</span> TamayoLeivaJ<br><br> 
-                     Source: Ergast API")
+                  <span style='font-family: \"Font Awesome 5 Brands\"'>&#xf09b;</span> TamayoLeivaJ<br><br> 
+                  Source: Emmys")
 
 #### Progress ####
-ggsave("./2021/2021_Week_039/Plots/2021_Week_039.png", dpi = 300, scale = 1, width = 12, height = 8, units = c("in"))
+ggsave("./2021/2021_Week_039/Plots/2021_Week_039.png", plot_emmys, dpi = 320, scale = 1, width = 8, height = 10, units = c("in"))
